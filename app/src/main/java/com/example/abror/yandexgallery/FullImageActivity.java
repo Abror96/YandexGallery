e6c72359eb9edf49f4270618936e32481a1ed8ec
Abror <abbasov894@gmail.com>
2018-04-22 00:20:35 +0500

    Картинка в новом окне
    added new Activity FullImageActivity for full screen images
    added opportunity for swiping images

---
+++ app/src/main/java/com/example/abror/yandexgallery/FullImageActivity.java
@@ -0,0 +1,66 @@
+package com.example.abror.yandexgallery;
+
+import android.annotation.SuppressLint;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    private List<String> images;
+    private int image_id;
+
+    private ViewPager imagePager;
+    private ImagePagerAdapter imagePagerAdapter;
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image);
+
+        // Заменяем текст в action bar на номер картинки с общим кол-вом картинок
+        String imageId = getIntent().getStringExtra("imageId");
+        image_id = Integer.parseInt(imageId);
+        String listItems = getIntent().getStringExtra("image_list");
+        images = Arrays.asList(listItems.split("\\s*,\\s*"));
+
+        imagePager = findViewById(R.id.imagesPager);
+        imagePagerAdapter = new ImagePagerAdapter(this, images);
+        imagePager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                getSupportActionBar().setTitle(position+1 + " из " + images.size());
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        imagePager.setAdapter(imagePagerAdapter);
+        imagePager.setCurrentItem(image_id);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/abror/yandexgallery/ImagePagerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.abror.yandexgallery;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImagePagerAdapter extends PagerAdapter {
+
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+    private List<String> images_list;
+
+
+    public ImagePagerAdapter(Context c, List<String> images) {
+        ctx = c;
+        images_list = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images_list.size();
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        Log.d("fullimage abror", String.valueOf(position));
+
+        View itemView = layoutInflater.inflate(R.layout.custom_swipe_item, container, false);
+        ImageView imageView = itemView.findViewById(R.id.fullImage);
+        Picasso.get().load(images_list.get(position)).into(imageView);
+        container.addView(itemView);
+
+        //((FullImageActivity) ctx).getSupportActionBar().setTitle(position + " из " + images_list.size());
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return (view == object);
+    }
+
+}
