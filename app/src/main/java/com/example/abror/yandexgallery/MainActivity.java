2e128e933ad09e1a85f3ad101870eb9b4856ddc9
Abror <abbasov894@gmail.com>
2018-04-24 00:11:45 +0500

    Проверка интернет соединения
    added the opportunity to check the internet connection, if there is no internet access, the app will show the appropriate picture

--- app/src/main/java/com/example/abror/yandexgallery/MainActivity.java
+++ app/src/main/java/com/example/abror/yandexgallery/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.abror.yandexgallery;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,17 +26,24 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private RecyclerView imagesView;
     private RecyclerView.Adapter adapter;
 
     private List<Images> imageItems;
 
+    private SwipeRefreshLayout swipeContainer;
+
+    private ImageView noInternetIc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeContainer = findViewById(R.id.swipe_container);
+        swipeContainer.setOnRefreshListener(this);
 
         imagesView = findViewById(R.id.imagesView);
         imagesView.setHasFixedSize(true);
@@ -37,7 +51,22 @@ public class MainActivity extends AppCompatActivity {
 
         imageItems = new ArrayList<>();
 
+        noInternetIc = findViewById(R.id.no_internet_ic);
+
+        // Checking the internet connection
+        if(!isNetworkStatusAvialable (getApplicationContext())) {
+            imagesView.setVisibility(View.GONE);
+            noInternetIc.bringToFront();
+        }
+
         new ParseTask().execute();
+    }
+
+    @Override
+    public void onRefresh() {
+        new ParseTask().execute();
+        checkNetwork();
+
     }
 
 
@@ -51,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Void... params) {
             // получаем данные с внешнего ресурса
             try {
-                URL url = new URL("http://www.omdbapi.com/?apikey=9ae0c3b9&s=cars");
+                URL url = new URL("https://api-fotki.yandex.ru/api/users/abbasov894/favorites/?format=json");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -79,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String strJson) {
             super.onPostExecute(strJson);
 
-
+            imageItems.clear();
             JSONObject dataJsonObj = null;
             try {
 
@@ -87,13 +116,16 @@ public class MainActivity extends AppCompatActivity {
 
                 dataJsonObj = new JSONObject(strJson);
 
-                JSONArray images = dataJsonObj.getJSONArray("Search");
+                JSONArray images = dataJsonObj.getJSONArray("entries");
 
                 // 2. перебираем и выводим контакты каждого друга
                 for (int i = 0; i < images.length(); i++) {
-                    JSONObject image = images.getJSONObject(i);
+                    JSONObject images_obj = images.getJSONObject(i);
 
-                    String imageUrl = image.getString("Poster");
+                    JSONObject image = images_obj.getJSONObject("img");
+                    JSONObject image_size = image.getJSONObject("L");
+
+                    String imageUrl = image_size.getString("href");
 
                     Images imageItem = new Images(imageUrl);
 
@@ -106,6 +138,44 @@ public class MainActivity extends AppCompatActivity {
 
             adapter = new ImagesAdapter(imageItems, getApplicationContext());
             imagesView.setAdapter(adapter);
+
+            swipeContainer.setRefreshing(false);
         }
+    }
+
+    public void checkNetwork(){
+        if(isNetworkStatusAvialable (getApplicationContext())) {
+            imagesView.setVisibility(View.VISIBLE);
+            noInternetIc.setVisibility(View.GONE);
+        } else {
+            imagesView.setVisibility(View.GONE);
+            noInternetIc.setVisibility(View.VISIBLE);
+            noInternetIc.bringToFront();
+        }
+    }
+
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            if(netInfos != null)
+                if(netInfos.isConnected())
+                    return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkNetwork();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        checkNetwork();
     }
 }
