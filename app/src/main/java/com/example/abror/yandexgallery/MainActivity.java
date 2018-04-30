d71da40f44a5e2bab83811d0be8d6812d78c2b90
Abror <abbasov894@gmail.com>
2018-04-30 18:18:10 +0500

    Получение разрешений
    Get runtime permission to save images in device storage

--- app/src/main/java/com/example/abror/yandexgallery/MainActivity.java
+++ app/src/main/java/com/example/abror/yandexgallery/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.abror.yandexgallery;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -28,47 +37,122 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    private RecyclerView imagesView;
-    private RecyclerView.Adapter adapter;
+    private RecyclerView imagesRecyclerView;
 
-    private List<Images> imageItems;
+    private List<Images> thumbImageItemsList;
 
-    private SwipeRefreshLayout swipeContainer;
+    private SwipeRefreshLayout swipeRefreshContainer;
 
     private ImageView noInternetIc;
 
+    private int num_of_cols;
+
+    private String fullImageUrl = "";
+
+    public static ProgressDialog mProgressDialog;
+
+    // объявляем разрешение, которое нам нужно получить
+    private static final String WRITE_EXTERNAL_STORAGE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        swipeContainer = findViewById(R.id.swipe_container);
-        swipeContainer.setOnRefreshListener(this);
+        // проверяем разрешения на запись на память телефона
+        if (!isPermissionGranted(WRITE_EXTERNAL_STORAGE_PERMISSION)) {
+            requestPermission(WRITE_EXTERNAL_STORAGE_PERMISSION);
+        }
 
-        imagesView = findViewById(R.id.imagesView);
-        imagesView.setHasFixedSize(true);
-        imagesView.setLayoutManager(new GridLayoutManager(this, 2));
+        swipeRefreshContainer = findViewById(R.id.swipe_container);
+        swipeRefreshContainer.setOnRefreshListener(this);
 
-        imageItems = new ArrayList<>();
+        imagesRecyclerView = findViewById(R.id.imagesView);
+        imagesRecyclerView.setHasFixedSize(true);
+        num_of_cols = 2;
+        imagesRecyclerView.setLayoutManager(new GridLayoutManager(this, num_of_cols));
+        imagesRecyclerView.setMotionEventSplittingEnabled(false);
+
+        thumbImageItemsList = new ArrayList<>();
 
         noInternetIc = findViewById(R.id.no_internet_ic);
 
-        // Checking the internet connection
-        if(!isNetworkStatusAvialable (getApplicationContext())) {
-            imagesView.setVisibility(View.GONE);
-            noInternetIc.bringToFront();
-        }
+        // вывод окна загрузки пока скачиваются картинки
+        mProgressDialog= new ProgressDialog(this);
+        mProgressDialog.setTitle("Загрузка изображений");
+        mProgressDialog.setMessage("Пожалуйста подождите пока картинки загружаются");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+        // проверка интернет соединения
+        checkNetwork();
+
+        // увеличение и уменьшение количества столбцов gridlyaout
+        final ScaleGestureDetector mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                if (detector.getCurrentSpan() > 150 && detector.getTimeDelta() > 150) {
+                    if (detector.getCurrentSpan() - detector.getPreviousSpan() < -1) {
+                        // увеличение количества столбцов
+                        if (num_of_cols < 4) num_of_cols++;
+                        imagesRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), num_of_cols));
+                        return true;
+                    } else if (detector.getCurrentSpan() - detector.getPreviousSpan() > 1) {
+                        // уменьшение количества столбцов
+                        if (num_of_cols > 2) num_of_cols--;
+                        imagesRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), num_of_cols));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        imagesRecyclerView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+        imagesRecyclerView.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mScaleGestureDetector.onTouchEvent(event);
+                return false;
+            }
+        });
 
         new ParseTask().execute();
     }
 
+    private boolean isPermissionGranted(String permission) {
+        // проверяем разрешение - есть ли оно у нашего приложения
+        int permissionCheck = ActivityCompat.checkSelfPermission(this, permission);
+        return permissionCheck == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        if (requestCode == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Разрешения получены", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Разрешения не получены", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void requestPermission(String permission) {
+        // запрашиваем разрешение
+        ActivityCompat.requestPermissions(this, new String[]{permission}, 2);
+    }
+
+    // действия при свайпе вниз (обновение)
     @Override
     public void onRefresh() {
         new ParseTask().execute();
         checkNetwork();
-
     }
-
 
     private class ParseTask extends AsyncTask<Void, Void, String> {
 
@@ -108,74 +192,74 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         protected void onPostExecute(String strJson) {
             super.onPostExecute(strJson);
 
-            imageItems.clear();
+            thumbImageItemsList.clear();
             JSONObject dataJsonObj = null;
             try {
 
-                Log.d("abror json", strJson);
-
                 dataJsonObj = new JSONObject(strJson);
-
                 JSONArray images = dataJsonObj.getJSONArray("entries");
 
-                // 2. перебираем и выводим контакты каждого друга
                 for (int i = 0; i < images.length(); i++) {
                     JSONObject images_obj = images.getJSONObject(i);
 
                     JSONObject image = images_obj.getJSONObject("img");
                     JSONObject image_size = image.getJSONObject("L");
+                    JSONObject imageFullSize = image.getJSONObject("XXXL");
 
                     String imageUrl = image_size.getString("href");
+                    fullImageUrl += imageFullSize.getString("href") + ", ";
 
                     Images imageItem = new Images(imageUrl);
 
-                    imageItems.add(imageItem);
-
+                    thumbImageItemsList.add(imageItem);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-            adapter = new ImagesAdapter(imageItems, getApplicationContext());
-            imagesView.setAdapter(adapter);
+            RecyclerView.Adapter adapter = new ImagesAdapter(thumbImageItemsList, fullImageUrl);
+            imagesRecyclerView.setAdapter(adapter);
 
-            swipeContainer.setRefreshing(false);
+            swipeRefreshContainer.setRefreshing(false);
         }
     }
 
+    // метод для показывания картинки "Нет интернет соединения" или
+    // списка картинок в зависимости от наличия интернет подключения
     public void checkNetwork(){
-        if(isNetworkStatusAvialable (getApplicationContext())) {
-            imagesView.setVisibility(View.VISIBLE);
+        if(isNetworkStatusAvailable (getApplicationContext())) {
+            imagesRecyclerView.setVisibility(View.VISIBLE);
             noInternetIc.setVisibility(View.GONE);
         } else {
-            imagesView.setVisibility(View.GONE);
+            imagesRecyclerView.setVisibility(View.GONE);
             noInternetIc.setVisibility(View.VISIBLE);
             noInternetIc.bringToFront();
         }
     }
 
-    public static boolean isNetworkStatusAvialable (Context context) {
+    // метод для проверки интернет соединения
+    public static boolean isNetworkStatusAvailable (Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null)
         {
             NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
             if(netInfos != null)
-                if(netInfos.isConnected())
-                    return true;
+                return netInfos.isConnected();
         }
         return false;
     }
 
+    //не дестроить приложение при нажатии на системную кнопку назад
     @Override
-    protected void onStart() {
-        super.onStart();
-        checkNetwork();
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
 
-    }
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
+        {
+            this.moveTaskToBack(true);
+            return true;
+        }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        checkNetwork();
+        return super.onKeyDown(keyCode, event);
     }
 }
